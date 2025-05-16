ae4dfb85b2c290b79f28c92aff1ae4c548cf90d1
nxb9626 <nxb9626@rit.edu>
2025-05-15 21:09:56 -0400

    0.0.1

---
+++ commands.sh
@@ -0,0 +1,8 @@
+# tim aliases
+alias start="ssh marble 'cd ~/Projects/tim && ./tog.sh start $1'"
+alias stop="ssh marble 'cd ~/Projects/tim && ./tog.sh stop $1'"
+alias view="ssh marble 'cd ~/Projects/tim && ./view.sh'"
+alias piv="ssh marble 'cd ~/Projects/tim && ./pivot.sh'"
+alias pivot="ssh marble 'cd ~/Projects/tim && ./pivot.sh'"
+alias timelog="zed ssh://marble/~/Projects/tim"
+
